0ad5f78492c9f7163eb884af1499e0e322d730fa
DESKTOP-T5GUPK4\user <jj@gmail.com>
2022-07-18 17:06:53 +0900

    initial environment setting

---
+++ src/main/java/com/memo/user/UserRestController.java
@@ -0,0 +1,24 @@
+package com.memo.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.memo.user.bo.UserBO;
+import com.memo.user.model.User;
+
+@RequestMapping("/user")
+@RestController
+public class UserRestController {
+	
+	@Autowired
+	private UserBO userBO;
+	
+	@RequestMapping("/user_list")
+	public List<User> userList(){
+		return userBO.getUserList();
+	}
+	
+}
